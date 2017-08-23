f11de6a3d2d5d56b9c5e19f3810941009150d3eb
dluu0630 <davidluu0630@gmail.com>
2017-08-22 22:53:22 -0700

    fixed timestamp on twitter call. got ride of deprecation

--- liri.js
+++ liri.js
@@ -1,9 +1,8 @@
 var fs = require("fs"); 
 var request = require("request");
-var keys = require("./keys.js");
 var liriArguments = process.argv[2];
-var twitter = require("twitter");
 
+//Switch Cases
 switch(liriArguments) {
     case "my-tweets":
     myTweets();
@@ -11,38 +10,34 @@ switch(liriArguments) {
     case "spotify-this-song":
     spotifyThisSong();
     break;
-};
+}
 
+// Twitter function
 function myTweets() {
+	// data from keys.js
+	var keys = require("./keys.js");
+	var twitter = require("twitter");
     var client = new twitter({
         consumer_key: keys.twitterKeys.consumer_key,
         consumer_secret: keys.twitterKeys.consumer_secret,
         access_token_key: keys.twitterKeys.access_token_key,
         access_token_secret: keys.twitterKeys.access_token_secret, 
-    });
+	});
     var params = {
 		screen_name: 'dluu0630',
 		count: '20',
 		trim_user: false,
-    }
-    client.get('statuses/user_timeline', params, function(error, timeline, response){
-		if(!error){
-			for(tweet in timeline){
-				//this creates the variable tdate which will store the result of the date from the twitter call for easier access later
-				var tDate = new Date(timeline[tweet].created_at);
-
-				//console.log all of the tweets organizing them by tweet# followed by the date of the tweet and finally the text of the tweet itself
-				console.log("Tweet #: " + (parseInt(tweet)+1) + " ");
-				console.log(tDate.toString().slice(0, 24) + " ");
-				console.log(timeline[tweet].text);
-				console.log("\n");
-
-				//append all of this information to the txt file 
-				fs.appendFile('log.txt', "Tweet #: " + (parseInt(tweet)+1) + "\n");
-				fs.appendFile('log.txt', timeline[tweet].text + "\n");
-				fs.appendFile('log.txt', "\n");
-
-			}
-		} 
-	});
+	}
+	// Gets search info. If error detected prints it else runs through for loop and prints tweets.
+	client.get('statuses/user_timeline', params, function(error, tweets) {
+		if (error) {
+		  console.log(error);
+		}
+		else {
+		  for(i=0 ; i < tweets.length ; i++){
+			console.log((i+1) + ". " + tweets[i].created_at + ": " + tweets[i].text);
+		  }
+		}
+	  });
+    
 }
