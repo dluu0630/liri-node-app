a3ceebefafd68a2358bdbbcfa7188288f656882f
dluu0630 <davidluu0630@gmail.com>
2017-08-24 00:00:54 -0700

    finished spotify, omdb, and dowhat functions

--- keys.js
+++ keys.js
@@ -1,8 +1,13 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: 'dPQ8VaYQjWf0KxscSDwGGF4Pn',
-  consumer_secret: '4BeHlJz7FZjHNDkM4et97XZPvxkNsZGqJs1ipMQRH6cXitzzsI',
-  access_token_key: '899876360365129728-NQNBFfajnskBYsUlbLrPdVDzVy6upHQ',
-  access_token_secret: 'kb7En4ivJoFFwAiQdhBJ6r9PRehAyEup7gWp2PDjx2179',
+    consumer_key: 'dPQ8VaYQjWf0KxscSDwGGF4Pn',
+    consumer_secret: '4BeHlJz7FZjHNDkM4et97XZPvxkNsZGqJs1ipMQRH6cXitzzsI',
+    access_token_key: '899876360365129728-NQNBFfajnskBYsUlbLrPdVDzVy6upHQ',
+    access_token_secret: 'kb7En4ivJoFFwAiQdhBJ6r9PRehAyEup7gWp2PDjx2179',
+}
+
+exports.spotifyKeys = {
+    client_id: "d258eb72e57d4b27a88934b4e6abefce",
+    client_secret: "1fb762077a084ce99c9169c6f253dc86"
 }
--- liri.js
+++ liri.js
@@ -1,43 +1,139 @@
-var fs = require("fs"); 
+var fs = require("fs");
 var request = require("request");
+// data from keys.js
+var keys = require("./keys.js");
+var twitter = require("twitter");
+var Spotify = require("node-spotify-api");
+var inquirer = require("inquirer");
 var liriArguments = process.argv[2];
+var actionArguments = process.argv[3];
 
 //Switch Cases
-switch(liriArguments) {
+switch (liriArguments) {
     case "my-tweets":
-    myTweets();
-    break;
+        myTweets();
+        break;
     case "spotify-this-song":
-    spotifyThisSong();
-    break;
+        spotifySong(actionArguments);
+        break;
+    case "movie-this":
+        movieThis(actionArguments);
+        break;
+    case "do-what-it-says":
+        doWhat();
+        break;
+
 }
 
 // Twitter function
 function myTweets() {
-	// data from keys.js
-	var keys = require("./keys.js");
-	var twitter = require("twitter");
     var client = new twitter({
         consumer_key: keys.twitterKeys.consumer_key,
         consumer_secret: keys.twitterKeys.consumer_secret,
         access_token_key: keys.twitterKeys.access_token_key,
-        access_token_secret: keys.twitterKeys.access_token_secret, 
-	});
+        access_token_secret: keys.twitterKeys.access_token_secret,
+    });
+
     var params = {
-		screen_name: 'dluu0630',
-		count: '20',
-		trim_user: false,
-	}
-	// Gets search info. If error detected prints it else runs through for loop and prints tweets.
-	client.get('statuses/user_timeline', params, function(error, tweets) {
-		if (error) {
-		  console.log(error);
-		}
-		else {
-		  for(i=0 ; i < tweets.length ; i++){
-			console.log((i+1) + ". " + tweets[i].created_at + ": " + tweets[i].text);
-		  }
-		}
-	  });
-    
+        screen_name: 'dluu0630',
+        count: '20',
+        trim_user: false,
+    }
+    // Gets search info. If error detected prints it else runs through for loop and prints tweets.
+    client.get('statuses/user_timeline', params, function(error, tweets) {
+        if (error) {
+            console.log(error);
+        } else {
+            for (i = 0; i < tweets.length; i++) {
+                console.log((i + 1) + ". " + tweets[i].created_at + ": " + tweets[i].text);
+            }
+        }
+    });
+
+}
+
+// Spotify function
+function spotifySong(trackName) {
+    var spotify = new Spotify({
+        id: keys.spotifyKeys.client_id,
+        secret: keys.spotifyKeys.client_secret
+    });
+    // if trackname not defined, default to Ace of Base The Sign
+    var trackName = process.argv[3];
+    if (!trackName) {
+        trackName = "Ace of Base The Sign";
+    }
+    // search track details and prints details
+    spotify.search({ type: "track", query: trackName }, function(err, data) {
+        if (!err) {
+            var trackDetails = data.tracks.items;
+            for (var i = 0; i < 1; i++) {
+                if (trackDetails[i] != undefined) {
+                    var spotifySearch =
+                        "Musician: " + trackDetails[i].artists[0].name + "\r\n" + "Track: " + trackDetails[i].name + "\r\n" + "Album: " + trackDetails[i].album.name + "\r\n" + "Preview: " + trackDetails[i].preview_url;
+                    console.log(spotifySearch);
+                }
+            }
+        } else {
+            console.log("Error :" + err);
+            return;
+        }
+    });
+};
+
+// Movie This function
+function movieThis() {
+    var movieName = process.argv[3];
+    var queryURL = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
+
+    request(queryURL, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var body = JSON.parse(body);
+            //prints movie details
+            console.log("Title: " + body.Title);
+            console.log("Release Year: " + body.Year);
+            console.log("IMdB Rating: " + body.imdbRating);
+            console.log("Country: " + body.Country);
+            console.log("Language: " + body.Language);
+            console.log("Plot: " + body.Plot);
+            console.log("Actors: " + body.Actors);
+            console.log("Rotten Tomatoes Rating: " + body.tomatoRating);
+            console.log("Rotten Tomatoes URL: " + body.tomatoURL);
+
+        } else {
+            console.log('Error occurred.');
+            if (movieName === undefined) {
+                movieName === "Mr. Nobody";
+            };
+            console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
+            console.log("It's on Netflix!");
+
+
+
+        }
+    });
+
+}
+
+// Do What It Says Function
+function doWhat() {
+	// reads  random.txt and do executes whats within file
+    fs.readFile("random.txt", 'utf8', function(err, data) {
+            fs.readFile("random.txt", "utf8", function(error, data) {
+                if (!error) {
+                    doWhatResults = data.split(",");
+                    spotifySong(doWhatResults[0], doWhatResults[1]);
+                } else {
+                    console.log("Error occurred" + error);
+                }
+            });
+        },
+        // reads and writes from log.txt then prints to to terminal 
+        function log(logResults) {
+            fs.appendFile("log.txt", logResults, (error) => {
+                if (error) {
+                    throw error;
+                }
+            })
+        })
 }
